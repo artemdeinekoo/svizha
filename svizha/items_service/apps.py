f3b1b42e860c0463b1e79b3ad14449a36057b4c9
Artem Deineko <artemdeinekoo@gmail.com>
2023-01-04 12:50:18 +0200

    Initial commit.

---
+++ svizha/items_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ItemsServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'items_service'
---
+++ svizha/svizha/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for svizha project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'svizha.settings')
+
+application = get_wsgi_application()
